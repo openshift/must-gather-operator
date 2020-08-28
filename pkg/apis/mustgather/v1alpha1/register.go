a8201cab16e62d456150aa31ab8566712ff19aeb
Dustin Row <drow@redhat.com>
2020-08-28 15:04:41 -0700

    Fix groupName

--- pkg/apis/mustgather/v1alpha1/register.go
+++ pkg/apis/mustgather/v1alpha1/register.go
@@ -2,7 +2,7 @@
 
 // Package v1alpha1 contains API Schema definitions for the openshift v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
-// +groupName=openshift.redhat.io
+// +groupName=managed.openshift.io
 package v1alpha1
 
 import (
