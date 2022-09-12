c4de1d9e693f97bd93ec98ec690f8112a631e734
jwai7 <jwai@redhat.com>
2022-09-12 10:59:02 +1000

    Migrated mustgather controllers and main.go Made local copy of reconcilerbase functions

--- pkg/apis/mustgather/v1alpha1/doc.go
+++
@@ -1,4 +0,0 @@
-// Package v1alpha1 contains API Schema definitions for the mustgather v1alpha1 API group
-// +k8s:deepcopy-gen=package,register
-// +groupName=managed.openshift.io
-package v1alpha1
