d57922beb6fba42e8c9e3cb3d7ad251ac53d90bf
Swarup Ghosh <swghosh@redhat.com>
2025-09-09 12:57:00 +0530

    api: set internalUser to false by default
    Signed-off-by: Swarup Ghosh <swghosh@redhat.com>

--- api/v1alpha1/mustgather_types.go
+++ api/v1alpha1/mustgather_types.go
@@ -17,8 +17,8 @@ limitations under the License.
 package v1alpha1
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
@@ -57,7 +57,7 @@ type MustGatherSpec struct {
 
 	// A flag to specify if the upload user provided in the caseManagementAccountSecret is a RH internal user.
 	// See documentation for further information.
-	// +kubebuilder:default:=true
+	// +kubebuilder:default:=false
 	InternalUser bool `json:"internalUser,omitempty"`
 }
 
