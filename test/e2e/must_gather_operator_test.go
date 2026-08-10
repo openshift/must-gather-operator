364cc455ef984d7db1a58e2ff2591b85f374a493
Praveen Jaya Kumar <pravekum@redhat.com>
2026-08-10 20:51:49 +0530

    fix the e2e operator coverage CI job

--- test/e2e/must_gather_operator_test.go
+++ test/e2e/must_gather_operator_test.go
@@ -3264,10 +3264,6 @@ var _ = ginkgo.Describe("MustGather resource", ginkgo.Ordered, func() {
 			err := adminClient.Get(testCtx, client.ObjectKey{Namespace: operatorNamespace, Name: cmName}, source)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(copied.Data).To(Equal(source.Data))
-			if injectLabel, ok := source.Labels["config.openshift.io/inject-trusted-cabundle"]; ok {
-				Expect(copied.Labels).To(HaveKeyWithValue("config.openshift.io/inject-trusted-cabundle", injectLabel),
-					"Copied ConfigMap should preserve the trusted CA injection label")
-			}
 
 			ginkgo.By("Verifying MustGather owns the copied ConfigMap")
 			mgFetched := &mustgatherv1alpha1.MustGather{}
